1592aeb5d009ef9ed916725e7129a53b5cddab69
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-08-09 09:16:19 +0300

    Initial Commit

---
+++ LangDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LangDiary
+//
+//  Created by Роман on 09.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
